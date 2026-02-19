dfd5df8a2dd8d1b7eb9d3fd5a8649f5b571b8e94
Shyue Ping Ong <shyuep@gmail.com>
2026-02-19 13:04:03 +0800

    Initial version - not working.

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+import tempfile
+from typing import TYPE_CHECKING
+
+import pytest
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+
+@pytest.fixture(autouse=True)
+def setup_teardown() -> Generator:
+    """Use tempdir for all tests."""
+    cwd = os.getcwd()
+    with tempfile.TemporaryDirectory() as tmpdir:
+        os.chdir(tmpdir)
+        yield tmpdir
+        os.chdir(cwd)
+
+
+@pytest.fixture(autouse=True)
+def test_files_dir() -> Path:
+    return Path(__file__).parent.parent / "pymatgen-test-files"
+    
